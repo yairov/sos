314eaf8a9414859ec59751dc2e739bdd369b1a10
yair-o-lottosheli <yairo@harmon.ie>
2025-07-06 11:24:25 +0300

    s

--- commands.bundle.js
+++ commands.bundle.js
@@ -7,7 +7,22 @@ function onMessageSendHandler(event) {
   });
 }
 
+function OnMessageComposeHandler(event) {
+  Office.context.mailbox.item.notificationMessages.addAsync('fd90eb33431b46f58a68720c36154b4a', {
+    type: 'insightMessage',
+    message: 'You can upload this email to harmon.ie',
+    icon: 'Icon.16x16',
+    actions: [
+      {
+        actionType: 'showTaskPane',
+        actionText: 'Upload email to harmon.ie',
+      },
+    ],
+  });
+}
+
 window.onMessageSendHandler = onMessageSendHandler;
+window.OnMessageComposeHandler = OnMessageComposeHandler;
 Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
-Office.actions.associate("OnMessageComposeHandler", onMessageSendHandler);
+Office.actions.associate("OnMessageComposeHandler", OnMessageComposeHandler);
 

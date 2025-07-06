b7a46c4faa22973ccc369464a4742ca4d1a82c57
yair-o-lottosheli <yairo@harmon.ie>
2025-07-06 11:23:05 +0300

    s

--- commands.bundle.js
+++ commands.bundle.js
@@ -1,9 +1,13 @@
-(() => {
-  'use strict';
-  Office.actions.associate('OnMessageComposeHandler', (async function(e) {
-    var t, s;
-    null === (s = null === (t = Office.context.mailbox) || void 0 === t ? void 0 : t.item) || void 0 === s || s.subject.setAsync('Set by an event-based add-in!', { asyncContext: e }, (function(e) {
-      e.status !== Office.AsyncResultStatus.Succeeded && console.error(`Failed to set subject: ${JSON.stringify(e.error)}`), e.asyncContext.completed();
-    }));
-  }));
-})();
+
+function onMessageSendHandler(event) {
+  event.completed({
+    allowEvent: false,
+    errorMessage: 'hello from harmon.ie',
+    sendModeOverride: 'PromptUser',
+  });
+}
+
+window.onMessageSendHandler = onMessageSendHandler;
+Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
+Office.actions.associate("OnMessageComposeHandler", onMessageSendHandler);
+
